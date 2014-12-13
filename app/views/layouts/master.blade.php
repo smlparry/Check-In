7ba9e5d8032dddacb808512ff306c0ea8e94f7c0
smlparry <samuel.parry@bigpond.com>
2014-12-13 13:04:06 +1100

    Helper links

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -10,9 +10,10 @@
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
         <!-- Place favicon.ico in the root directory -->
 	   
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/style.css">
-        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+       {{ HTML::style('css/bootstrap.min.css') }}
+       {{ HTML::style('css/style.css') }}
+       {{ HTML::script('js/modernizr-2.8.3.min.js') }}
+
     </head>
     <body>
         <!--[if lt IE 8]>
@@ -28,10 +29,11 @@
 
         @yield("content")
 
-
+    
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
-        <script src="js/main.js"></script>
+        <script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+        {{ HTML::script('js/main.js') }}
+
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
